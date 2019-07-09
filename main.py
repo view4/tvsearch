555610029aee3464faa547aa8b72522857a7f147
Gabriel Morris <morrisgaby987@googlemail.com>
2019-07-09 14:41:50 +0300

    adding code for displaying the search results

--- main.py
+++ main.py
@@ -26,36 +26,74 @@ def index():
 @route('/browse')
 def browse():
     sectionTemplate = "./templates/browse.tpl"
-    ##data=[]
-    ##for show in utils.AVAILABLE_SHOWS:
-       ## data.append(show)
-    ##data=json.load(data)
+    data=[]
+    for show in utils.AVAILABLE_SHOWS:## loop to collect all the data and store in the 'data' array.
+        showInfo=utils.getJsonFromFile(show)
+        data.append(json.loads(showInfo))
+    print(data)
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = data)##data passed to the template page here
 
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
 @route('/search')
 def search():
     sectionTemplate = "./templates/search.tpl"
+#   query="";
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
-result={
-    "name":"hey"
-}
-""""
-#creates route for each of the tv shows
-for show in utils.AVAILABLE_SHOWS:
-    @route('/show/<param>'.format(show))
-    def shows():
-        sectionTemplate = "./templates/show.tpl" #should be show, but does not work at the moment
-        print(utils.getJsonFromFile(show))
-        print(show)
-        data=utils.getJsonFromFile(show)
-        data=json.loads(data)
-        return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = data)
-"""
+
+
+"""query="star"
+data=[]
+for show in utils.AVAILABLE_SHOWS:## loop to collect all the data and store in the 'data' array.
+
+        showInfo=utils.getJsonFromFile(show)
+        showInfo=json.loads(showInfo)
+        episodes=showInfo["_embedded"]["episodes"]
+        for episode in episodes:##looping through each of the episodes in each series
+            name =episode["name"]
+            summary=episode["summary"]
+            print(name)
+            print(summary)
+            if name is not None and summary is not None:
+                if query in name or query in summary:
+                    ##return the episode if it contains the search parameter
+                    data.append(episode)
+
+        print(data)"""
+@route('/search', method='POST')
+def default():
+    query =request.forms.get('q')
+    print(query)
+    data=[]
+    for show in utils.AVAILABLE_SHOWS:## loop to collect all the data and store in the 'data' array.
+
+        showInfo=utils.getJsonFromFile(show)
+        showInfo=json.loads(showInfo)
+        episodes=showInfo["_embedded"]["episodes"]
+        for episode in episodes:##looping through each of the episodes in each series
+                name =episode["name"]
+                summary=episode["summary"]
+                print(name)
+                print(summary)
+                if name is not None and summary is not None:
+                    if query in name or query in summary:
+                        ##return the episode if it contains the search parameter
+
+                        object={
+                            "showid":show,
+                            "episodeid":episode["id"],
+                            "text":name,
+
+                        }
+                        data.append(object)
+    print(data)
+    sectionTemplate = "./templates/search_result.tpl"
+
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = data, query=query, results=data)##data passed to the template page here
+
+
+
 @route('/show/<show>')
 def shows(show):
     sectionTemplate = "./templates/show.tpl" #should be show, but does not work at the moment
-    print(utils.getJsonFromFile(show))
-    print(show)
     data=utils.getJsonFromFile(show)
     data=json.loads(data)
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = data)
